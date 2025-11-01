91a213e42cd5c1e0d014b508f87cdb23d64a16ce
RashmithaBRamesh <153580913+RashmithaBRamesh@users.noreply.github.com>
2025-11-01 13:01:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,19 @@ fruit_options = [row['FRUIT_NAME'] for row in my_dataframe.collect()]  # convert
 # Multiselect for ingredients
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    fruit_options,
+    my_dataframe,
     max_selections=5
 )
 
 # Only proceed if fruits are selected
 if ingredients_list:
-    ingredients_string = " ".join(ingredients_list)
+    ingredients_string = " "
 
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f"{fruit_chosen} Nutrition Information")
 
         try:
